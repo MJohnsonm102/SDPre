9115b1330d51aaeaa16682ae8d340b73c75bccd7
Michael Johnson <michaeljohnsonbrass@gmail.com>
2024-01-28 03:21:08 -0700

    completed Obj 2 Lab 3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int yearBorn = 1991;
+      String month = "February";
+        boolean weekEnd = true;
+
+System.out.println(yearBorn);
+System.out.println(month);
+System.out.println(weekEnd);
+
+
+  }
+}
