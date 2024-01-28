d6d74c49d779434f5ec9e1f7f8e44293a71d24f3
Michael Johnson <michaeljohnsonbrass@gmail.com>
2024-01-28 02:58:53 -0700

    completed obj 2 Lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Michael Johnson";
+    int age = 32;
+    String state = "South Carolina";
+      System.out.println(name);
+        System.out.println(age);
+          System.out.println(state);
+
+    
+  }
+}
