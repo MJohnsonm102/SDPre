63054ff6c470e43a4ab39cfd5bea9b4b91975979
Michael Johnson <michaeljohnsonbrass@gmail.com>
2024-01-29 18:31:25 -0700

    completed obj 3 lab 2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,14 @@
+public class Objective3Lab2 {
+
+public static void main(String[] args){
+ double side1, side2, hypotenuse;
+
+side1 = Math.sqrt(10 * 10);
+side2 = Math.sqrt(8 * 8);
+
+hypotenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2,2));
+
+
+ System.out.println(hypotenuse); 
+	}
+}
